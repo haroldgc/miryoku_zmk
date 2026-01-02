cd705a91382d9f7a97722a458512c1b3ae565caf
haroldgc <harold.gc@gmail.com>
2026-01-01 20:03:50 -0600

    Add extra layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,2 +1,30 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_KLUDGE_DOUBLETAPBOOT
+
+#if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
+
+#define MIRYOKU_LAYER_EXTRA \
+&kp TAB,           &kp Q,             &kp W,             &kp E,             &kp R,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+&kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
+&kp LCTRL,         &kp Z,             &kp X,             &kp C,             &kp V,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &kp LALT,          &kp SPC,           &mo U_TAP,         &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+
+#define XXX &none
+
+#define MIRYOKU_LAYERMAPPING_EXTRA( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+XXX &ext_power EP_ON   K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+XXX &ext_power EP_OFF  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX &ext_power EP_TOG  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+                            K32  K33  K34       K35  K36  K37
+
+#endif
+
